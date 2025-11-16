06de0ad4714f408c3a9e4c313d575ff9799bde1a
kimgijung <kgj0629@gmail.com>
2025-11-17 01:28:20 +0900

    [D2] Title: 쉬운 거스름돈, Time: 60 ms, Memory: 52,608 KB -BaekjoonHub

---
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.py
@@ -0,0 +1,18 @@
+T=int(input())
+
+
+for tc in range(1,T+1):
+
+    N = int(input())
+
+    money = [50000, 10000, 5000, 1000, 500, 100, 50,10]
+    result = []
+    for m in money:
+        result.append(N//m)
+        N%=m
+
+    print(f'#{tc}')
+    print(*result)
+
+
+
