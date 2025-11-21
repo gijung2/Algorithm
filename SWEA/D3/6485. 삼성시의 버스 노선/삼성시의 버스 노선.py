fc5d398a19f876741171c43a0c7af9dfd78390f2
kimgijung <kgj0629@gmail.com>
2025-11-22 01:46:05 +0900

    [D3] Title: 삼성시의 버스 노선, Time: 107 ms, Memory: 61,440 KB -BaekjoonHub

---
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
@@ -0,0 +1,19 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    cnts = [0] * 5001
+
+    for _ in range(N):
+        S, E = map(int, input().split())
+        for i in range(S, E+1):
+            cnts[i] += 1
+
+    P = int(input())
+    sols = []
+
+    for _ in range(P):
+        t = int(input())
+        sols.append(cnts[t])
+
+    print(f"#{tc}", *sols)
